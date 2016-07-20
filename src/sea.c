9a670c0bf570a661a1add8a4e501710dab8bcfbe
Ben LeMasurier <ben.lemasurier@gmail.com>
2016-07-19 21:36:01 -0600

    refactor account, general cleanup
    account:
    - split account related functions into separate files
    - include all available json fields in account type
    
    build:
    - enable debugging symbols
    - be pedantic
    
    notes:
    - I've used standard include guards rather than '#pragma once'. I'm open
      to change but I think this is a +1 for portability.

--- src/sea.c
+++ src/sea.c
@@ -2,25 +2,17 @@
  * Copyright (c) 2016 Sean Swehla <sean@sweh.la>
  */
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
+
 #include "sea.h"
-
-#include <jansson.h>
-#include <curl/curl.h>
-
-#define URL_FORMAT   "https://api.digitalocean.com/v2/account"
-#define URL_SIZE     256
+#include "request.h"
+#include "account.h"
 
 int main(int argc, char *argv[])
 {
-	char *text;
-	char url[URL_SIZE];
-	int ret;
-
-	json_t *root;
-	json_error_t error;
-
 	if(argc != 2)
 	{
 		fprintf(stderr, "usage: %s account\n\n", argv[0]);
@@ -28,71 +20,27 @@ int main(int argc, char *argv[])
 		return 2;
 	}
 
-	ret = 0;
-
-	//snprintf(url, URL_SIZE, URL_FORMAT, argv[1], argv[2]);
-	snprintf(url, URL_SIZE, URL_FORMAT);
-
-	text = request(url);
-	if(!text)
-		return 1;
-
-	root = json_loads(text, 0, &error);
-	free(text);
-
-	if(!root)
+	struct account *a = account_new();
+	if(a == NULL)
 	{
-		fprintf(stderr, "error: on line %d: %s\n", error.line, error.text);
-		return 1;
+		account_free(a);
+		perror("account: ");
+		return EXIT_FAILURE;
 	}
 
-	if(!json_is_object(root))
-	{
-		fprintf(stderr, "error: root is not an object\n");
-		ret = 1;
-		goto finish;
-	}
+	if(!account_get(a))
+		return EXIT_FAILURE;
 
-	json_t *data, *email, *status, *droplet_limit;
+	printf("Email: %s (verified: %s)\nStatus: %s\nStatus Message: %s\nUUID: %s\nDroplet Limit: %d\nFloating IP Limit: %d\n",
+			a->email,
+			(a->email_verified) ? "true" : "false",
+			a->status,
+			a->status_message,
+			a->uuid,
+			a->droplet_limit,
+			a->floating_ip_limit);
 
-	data = json_object_get(root, "account");
-	if(!json_is_object(data))
-	{
-		fprintf(stderr, "error: account data is not an object\n");
-		ret = 1;
-		goto finish;
-	}
+	account_free(a);
 
-	email = json_object_get(data, "email");
-	if(!json_is_string(email))
-	{
-		fprintf(stderr, "error: email is not a string\n");
-		ret = 1;
-		goto finish;
-	}
-
-	status = json_object_get(data, "status");
-	if(!json_is_string(status))
-	{
-		fprintf(stderr, "error: status is not an string\n");
-		ret = 1;
-		goto finish;
-	}
-
-	droplet_limit = json_object_get(data, "droplet_limit");
-	if(!json_is_integer(droplet_limit))
-	{
-		fprintf(stderr, "error: droplet_limit is not a number\n");
-		ret = 1;
-		goto finish;
-	}
-
-	printf("Email: %s\nStatus: %s\nDroplet Limit: %d\n",
-			json_string_value(email),
-			json_string_value(status),
-			(int)json_integer_value(droplet_limit));
-
-finish:
-	json_decref(root);
-	return ret;
+	return EXIT_SUCCESS;
 }
